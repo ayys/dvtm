739d08b3a4d407fa9e4253d99d45a82f542375e2
Ayush Jha <ayush@wasmer.io>
2024-05-06 18:05:00 +0545

    update flake.nix to fix naming issues

--- flake.nix
+++ flake.nix
@@ -1,27 +1,36 @@
 {
   description = "ayys's fork of dvtm";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flakeutils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
-    {
-      defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
-        name = "dvtm";
-        src = ./.;
-        buildInputs = [
-          pkgs.gcc pkgs.ncurses
-        ];
-        buildPhase = ''
+    { self, nixpkgs, flakeutils }:
+      flakeutils.lib.eachDefaultSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          packages = rec {
+            dvtm = pkgs.stdenv.mkDerivation {
+              name = "dvtm";
+              src = ./.;
+              buildInputs = [
+                pkgs.gcc  pkgs.ncurses
+              ];
+              buildPhase = ''
           make
         '';
-        installPhase = ''
+              installPhase = ''
           TERMINFO="$out" make install DESTDIR="$out" PREFIX=""
         '';
-      };
-
-    };
+            };
+            default = dvtm;
+          };
+          apps = rec {
+            dvtm = flakeutils.lib.mkApp { drv = self.packages.${system}.dvtm; };
+            default = dvtm;
+          };
+        }
+      );
 }
